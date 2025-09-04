8b03bbca977eda1e534d4ea9b5a913050208b664
noel <noelngui1@gmail.com>
2025-09-04 12:02:44 -0700

    adding options for workshop input

--- force-app/main/default/lwc/programManager/programManager.js
+++ force-app/main/default/lwc/programManager/programManager.js
@@ -1,8 +1,10 @@
 // programManager.js
-import { LightningElement, track } from 'lwc';
+import { LightningElement, track, api } from 'lwc';
 
 export default class ProgramManager extends LightningElement {
   @track activeTab = 'workshops';
+  @api showCollegeSuccess;
+  @api showPPP;
 
   handleTabChange(event) {
     this.activeTab = event.target.value;
